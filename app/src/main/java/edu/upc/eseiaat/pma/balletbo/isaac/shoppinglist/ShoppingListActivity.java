b13078c1577c83f921202ea8e4b0de693ea3196f
isaac10396 <isaacballetbocano@gmail.com>
2017-10-20 12:40:44 +0200

    ShoppingList

---
+++ app/src/main/java/edu/upc/eseiaat/pma/balletbo/isaac/shoppinglist/ShoppingListActivity.java
@@ -0,0 +1,42 @@
+package edu.upc.eseiaat.pma.balletbo.isaac.shoppinglist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ShoppingListActivity extends AppCompatActivity {
+
+    private ArrayList<String> itemList;
+    private ArrayAdapter<String> adapter;
+
+    private ListView list;
+    private Button btn_add;
+    private EditText edit_item;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+
+        list = (ListView) findViewById(R.id.list);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        edit_item = (EditText) findViewById(R.id.edit_item);
+
+        itemList = new ArrayList<>();
+        itemList.add("Patatas");
+        itemList.add("Papel WC");
+        itemList.add("Zanahorias");
+        itemList.add("Copas Danone");
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemList);
+
+
+        list.setAdapter(adapter);
+    }
+}
